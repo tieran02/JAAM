315bf44a233c7628db27053ca0f07e37649a57a8
tieran02 <tieran.wightman:gmail.com>
2023-02-19 10:08:06 +0000

    multithread job pool

--- converter/src/main.cpp
+++ converter/src/main.cpp
@@ -14,10 +14,104 @@
 
 #include "util.h"
 #include "modelConverter.h"
+#include <queue>
+#include <thread>
+#include <functional>
+#include <mutex>
+#include <chrono>
+
 using namespace Asset;
 
 
 static uint16_t textureChecksum = 0;
+
+namespace
+{
+	class JobPool
+	{
+	public:
+		JobPool(int threadCount);
+		~JobPool();
+		void push(std::function<void()> func);
+		void done();
+	private:
+
+		void work();
+
+		std::queue<std::function<void()>> m_function_queue;
+		std::mutex m_lock;
+		std::condition_variable m_data_condition;
+		std::atomic<bool> m_accept_functions;
+		std::vector<std::thread> m_workerThreads;
+	};
+
+	JobPool::JobPool(int threadCount) : m_function_queue(), m_lock(), m_data_condition(), m_accept_functions(true)
+	{
+		m_workerThreads.resize(threadCount);
+		for (uint32_t  i = 0; i < threadCount; i++)
+		{
+			m_workerThreads.at(i) = std::thread(&JobPool::work, this);
+		}
+	}
+
+	JobPool::~JobPool()
+	{
+	}
+
+	void JobPool::push(std::function<void()> func)
+	{
+		//If there are no worker threads just call the func (single threaded)
+		if (m_workerThreads.empty())
+			func();
+
+		std::unique_lock<std::mutex> lock(m_lock);
+		m_function_queue.push(func);
+		// when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+		lock.unlock();
+		m_data_condition.notify_one();
+	}
+
+	void JobPool::done()
+	{
+		std::unique_lock<std::mutex> lock(m_lock);
+		m_accept_functions = false;
+		lock.unlock();
+		// when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+		m_data_condition.notify_all();
+		//notify all waiting threads.
+
+		for (auto& thread : m_workerThreads)
+		{
+			thread.join();
+		}
+	}
+
+	void JobPool::work()
+	{
+		std::function<void()> func;
+		while (true)
+		{
+			{
+				std::unique_lock<std::mutex> lock(m_lock);
+				m_data_condition.wait(lock, [this]()
+					{
+						return !m_function_queue.empty() || !m_accept_functions;
+					});
+
+				if (!m_accept_functions && m_function_queue.empty())
+				{
+					//lock will be release automatically.
+					//finish the thread loop and let it join in the main thread.
+					return;
+				}
+				func = m_function_queue.front();
+				m_function_queue.pop();
+				//release the lock
+			}
+			func();
+		}
+	}
+}
 
 
 bool ConvertImage(const fs::path& input, const fs::path& output, const fs::path& rootPath)
@@ -52,13 +146,17 @@ bool ConvertImage(const fs::path& input, const fs::path& output, const fs::path&
 
 int main(int argc, char** argv)
 {
-	Asset::TextureManagerBasic textureManager;
+	/*Asset::TextureManagerBasic textureManager;
 	AssetHandle textureHandle = textureManager.Load("data/models/sponza/textures/background.tx");
 	AssetHandle textureHandleLoad1 = textureManager.Load("data/models/sponza/textures/background.tx");
 
 	Asset::TextureInfo* get = textureManager.Get(textureHandle);
 	auto* userData = textureManager.GetUserData(textureHandle);
-	std::cout << (int)get->textureFormat;
+	std::cout << (int)get->textureFormat;*/
+
+	int num_threads = std::thread::hardware_concurrency();
+	std::cout << "number of threads = " << num_threads << std::endl;
+	JobPool jobPool(num_threads);
 
 	for (int i = 0; i < argc; ++i)
 		std::cout << argv[i] << '\n';
@@ -67,6 +165,8 @@ int main(int argc, char** argv)
 	const fs::path output{ argv[2] };
 
 	std::cout << "loading asset directory at " << path << std::endl;
+
+	auto start = std::chrono::high_resolution_clock::now();
 
 	for (auto& p : fs::recursive_directory_iterator(path))
 	{
@@ -80,14 +180,28 @@ int main(int argc, char** argv)
 			std::cout << "found a texture" << p << std::endl;
 
 			newpath.replace_extension(".tx");
-			ConvertImage(p.path(), newpath, rootPath);
+			jobPool.push([=]
+				{
+					ConvertImage(p.path(), newpath, rootPath);
+				});
+			
 		}
 		if (p.path().extension() == ".obj")
 		{
 			std::cout << "found a mesh" << p << std::endl;
 
 			newpath.replace_extension(".mesh");
-			ConvertMesh(p.path(), newpath, rootPath);
+			jobPool.push([=]
+				{
+					ConvertMesh(p.path(), newpath, rootPath);
+				});
 		}
 	}
+
+	jobPool.done();
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto microseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << microseconds.count() << "ms to package\n";
+
 }
